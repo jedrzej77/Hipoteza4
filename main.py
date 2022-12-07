b8358436fa64180e3d56b0903de7ebe630ab2ae3
uczen <uczen@ckziu.jaworzno.pl>
2022-12-07 10:09:50 +0100

    1

---
+++ main.py
@@ -0,0 +1,10 @@
+alfabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+class klasa:
+    a=input("podaj hasło")
+    print(a)
+    for i in range a.lenght
+
+    print(klasa)
+
+
