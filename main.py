c68521ff358fc4acb8056e1502e88141973d397e
uczen <uczen@ckziu.jaworzno.pl>
2022-12-09 10:57:06 +0100

    added list

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 alfabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-class klasa:
-    a=input("podaj hasło")
-    print(a)
-    for i in range a.lenght
+a=input("podaj hasło: ")
+b=list(a)
+print(b)
+print(b[0])
+# print(len(a))
+for i in range(len(b)):
+    print("+")
+print(len(a))
 
-    print(klasa)
+
+
+
 
 
